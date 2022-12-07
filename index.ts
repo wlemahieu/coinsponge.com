cd43313077bb3d7cc95f4af63957cfdffb0942d1
Wesley LeMahieu <lateralis3k@gmail.com>
2022-12-07 11:57:49 -0800

    Use module-alias for compiled build paths

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import moduleAlias from 'module-alias';
+
+moduleAlias.addAliases({
+  '@config': `${__dirname}/config`,
+});
+
 import configExpress from '@config/express';
 import configSessions from '@config/sessions';
 import configSockets from '@config/sockets';
@@ -8,6 +14,6 @@ const [sessionParser] = configSessions();
 const [server, wss] = configSockets(app, sessionParser);
 configRoutes(app);
 
-server.listen(7777, '0.0.0.0', () => {
+server.listen(process.env.PORT || 7777, () => {
   console.log(`*~~% www.coinsponge.com %~~*`);
 });
